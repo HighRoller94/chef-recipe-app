ff0f7f6eaf810f02886d132aa3dac7452e23810f
Ash Bridges <ashbridges@virginmedia.com>
2021-12-16 15:20:05 +0000

    updated component for newsletter/sign up

--- src/components/Home/Newsletter.js
+++ src/components/Home/Newsletter.js
@@ -3,7 +3,20 @@ import React from 'react'
 function Newsletter() {
     return (
         <div className="newsletter__section">
-            
+            <div className="newsletter__left">
+                <h1>Sign up to our newsletter</h1>
+                <p>to receive weekly updates!</p>
+            </div>
+            <div className="newsletter__right">
+                <form onClassName="newsletter__form">
+                    <div className="text__field">
+                        <input type="text" name="email" required />
+                        <label htmlFor="email" className="label__name">
+                            <span className="content__name">email address</span>
+                        </label>
+                    </div>
+                </form>
+            </div>
         </div>
     )
 }
