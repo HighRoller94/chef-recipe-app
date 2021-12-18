7d6518e123fef9ad3f0539e32267d4e0e9fefdc8
Ash Bridges <ashbridges@virginmedia.com>
2021-12-18 12:59:59 +0000

    isolated pagination into seperate component

---
+++ src/components/Pagination.js
@@ -0,0 +1,44 @@
+import React, { useState, useEffect }from 'react'
+import { useHistory } from 'react-router-dom'
+
+import KeyboardArrowLeftIcon from '@material-ui/icons/KeyboardArrowLeft'
+import KeyboardArrowRightIcon from '@material-ui/icons/KeyboardArrowRight'
+
+function Pagination({ recipes, setRecipes, next, setNext, pages, prev, setPrev }) {
+    const history = useHistory()
+    const totalPageNumber = Math.ceil(pages/20);
+    const [currentPage, setCurrentPage] = useState(1);
+
+    const scrollToTop = () => {
+        window.scrollTo({
+            top: 0,
+            behaviour: 'smooth'
+        });
+    }
+
+    const getMoreRecipes = async () => {
+        const response = await fetch(`${next}`)
+        const data = await response.json();
+        console.log(data)
+        setRecipes(data.hits)
+        setCurrentPage(currentPage + 1)
+        setNext(data._links.next.href)
+        scrollToTop();
+    }
+
+    const getPreviousRecipes = async () => {
+        setRecipes(prev.hits)
+        setCurrentPage(currentPage - 1)
+        scrollToTop();
+    }
+
+    return (
+        <div className="pagination">
+            <KeyboardArrowLeftIcon onClick={getPreviousRecipes} />
+            <p>{currentPage} of {totalPageNumber}</p>
+            <KeyboardArrowRightIcon onClick={getMoreRecipes} />
+        </div>
+    )
+}
+
+export default Pagination
